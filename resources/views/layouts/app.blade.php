7fb0e06e40bf0844edc8ec3624a9c48d2cd44696
tri angga utama <trianggategarutama@gmail.com>
2019-04-26 09:09:57 +0800

    mengubah link asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Aplikasi Lelang Kayu</title>
   <!-- plugins:css -->
-  <link rel="stylesheet" href="{{ asset('/admin/vendors/mdi/css/materialdesignicons.min.css') }}">
-  <link rel="stylesheet" href="/admin/vendors/base/vendor.bundle.base.css">
+  <link rel="stylesheet" href="{{ asset('/admin/vendors/mdi/css/materialdesignicons.min.css')}}">
+  <link rel="stylesheet" href="{{asset('admin/vendors/base/vendor.bundle.base.css')}}">
   <!-- endinject -->
   <!-- plugin css for this page -->
   <link rel="stylesheet" href="{{ asset('/admin/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
@@ -169,24 +169,24 @@
   <!-- container-scroller -->
 
   <!-- plugins:js -->
-  <script src="/admin/vendors/base/vendor.bundle.base.js"></script>
+  <script src="{{asset('/admin/vendors/base/vendor.bundle.base.js')}}"></script>
   <!-- endinject -->
   <!-- Plugin js for this page-->
-  <script src="/admin/vendors/chart.js/Chart.min.js"></script>
-  <script src="/admin/vendors/datatables.net/jquery.dataTables.js"></script>
-  <script src="/admin/vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
+  <script src="{{asset('/admin/vendors/chart.js/Chart.min.js')}}"></script>
+  <script src="{{asset('/admin/vendors/datatables.net/jquery.dataTables.js')}}"></script>
+  <script src="{{asset('/admin/vendors/datatables.net-bs4/dataTables.bootstrap4.js')}}"></script>
   <!-- End plugin js for this page-->
   <!-- inject:js -->
-  <script src="/admin/js/off-canvas.js"></script>
-  <script src="/admin/js/hoverable-collapse.js"></script>
-  <script src="/admin/js/template.js"></script>
+  <script src="{{asset('/admin/js/off-canvas.js')}}"></script>
+  <script src="{{asset('/admin/js/hoverable-collapse.js')}}"></script>
+  <script src="{{asset('/admin/js/template.js')}}"></script>
   <!-- endinject -->
   <!-- Custom js for this page-->
-  <script src="/admin/js/dashboard.js"></script>
-  <script src="/admin/js/data-table.js"></script>
-  <script src="/admin/js/jquery.dataTables.js"></script>
-  <script src="/admin/js/dataTables.bootstrap4.js"></script>
-  <script src="/admin/js/file-upload.js"></script>
+  <script src="{{asset('/admin/js/dashboard.js')}}"></script>
+  <script src="{{asset('/admin/js/data-table.js')}}"></script>
+  <script src="{{asset('/admin/js/jquery.dataTables.js')}}"></script>
+  <script src="{{asset('/admin/js/dataTables.bootstrap4.js')}}"></script>
+  <script src="{{asset('/admin/js/file-upload.js')}}"></script>
   <!-- End custom js for this page-->
   <script>
         $(document).ready( function () {
