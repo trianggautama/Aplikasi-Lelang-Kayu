75721fc7b8a4ce547b196dedc8e65719b2feeb57
unknown <tomycules@gmail.com>
2019-06-24 00:18:48 +0800

    disabled link riwayat lelang

--- resources/views/layouts/peserta.blade.php
+++ resources/views/layouts/peserta.blade.php
@@ -131,7 +131,7 @@
             </a>
             <div class="collapse" id="ui-basic">
               <ul class="nav flex-column sub-menu">
-              <li class="nav-item"> <a class="nav-link" href="{{ route('karyawan_index') }}">Riwayat Lelang</a></li>
+              {{-- <li class="nav-item"> <a class="nav-link" href="{{ route('karyawan_index') }}">Riwayat Lelang</a></li> --}}
               </ul>
             </div>
           </li>
