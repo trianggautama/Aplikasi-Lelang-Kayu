e5817ae3fa57b1f6de1afe535142d5ee9770852a
unknown <tomycules@gmail.com>
2019-06-24 02:07:53 +0800

    add peserta detail view

---
+++ resources/views/admin/peserta_lelang_detail.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+@section('content')
+  <!-- partial -->
+  <div class="main-panel">
+    <div class="content-wrapper">
+      <div class="row">
+        <div class="col-md-12 grid-margin">
+          <div class="d-flex justify-content-between flex-wrap">
+            <div class="d-flex align-items-end flex-wrap">
+              <div class="mr-md-3 mr-xl-5">
+                <h2>Profile peserta,</h2>
+              </div>
+            </div>
+            <div class="d-flex justify-content-between align-items-end flex-wrap">
+            </div>
+          </div>
+        </div>
+      </div>
+            <div class="row">
+                    <div class="col-md-4 grid-margin stretch-card">
+                            <div class="card">
+                              <div class="card-body text-center">
+                                <p class="card-title">Foto peserta</p>
+                               <img src="{{asset('/images/peserta/'.$peserta->foto)}}" alt="" width="100%">
+                              </div>
+                            </div>
+                          </div>
+                    <div class="col-md-8 grid-margin stretch-card">
+                      <div class="card">
+                            <div class="card-body">
+                                    <h4 class="card-title">Biodata Peserta</h4>
+
+                                    <div class="template-demo">
+                                      <h4 class="card-title">Nama Peserta                    : {{$peserta->user->name}}</h4>
+                                      <h4 class="card-title">Alamat                  : {{$peserta->alamat}} </h4>
+                                      <h4 class="card-title">No Telp                 : {{$peserta->telepon}}</h4>
+                                      <h4 class="card-title">Email                :  {{ $peserta->user->email }}</h4>
+                                    </div>
+                                  </div>
+                                  <div class="card-footer">
+                                  <div class="text-right">
+                                            <a href="{{ route('peserta-lelang-edit', ['id' => IDCrypt::Encrypt( $peserta->id)])}}" class="btn btn-inverse-info"><i class=" mdi mdi-printer "></i> Cetak data</a>
+                                            <a href="{{ route('peserta-lelang-edit', ['id' => IDCrypt::Encrypt( $peserta->id)])}}" class="btn btn-inverse-primary"><i class=" mdi mdi-pencil "></i> Ubah data</a>
+                                            <a href="{{ route('peserta-lelang-index') }}" class="btn btn-inverse-danger"><i class=" mdi mdi-arrow-left-thick "></i> Kembali</a>
+                                       </div>
+                                  </div>
+                      </div>
+                    </div>
+
+                  </div>
+      </div>
+@endsection
