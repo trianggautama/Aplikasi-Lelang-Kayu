61237cca8b7c6e3dccb80048a42013d2fffb769f
unknown <tomycules@gmail.com>
2019-06-24 02:01:58 +0800

    make migration peserta table

---
+++ database/migrations/2019_06_23_162435_create_pesertas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePesertasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pesertas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedbigInteger('user_id');
+            $table->string('alamat')->length('25');
+            $table->string('telepon')->length(255);
+            $table->string('pekerjaan')->length(255);
+            $table->string('foto')->length('255')->default('default.png');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pesertas');
+    }
+}
