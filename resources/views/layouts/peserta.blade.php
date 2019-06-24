cda66161bfddb5a1776d71bccb1c2854ae6fc64c
unknown <tomycules@gmail.com>
2019-06-24 13:57:48 +0800

    fix route peserta

--- resources/views/layouts/peserta.blade.php
+++ resources/views/layouts/peserta.blade.php
@@ -112,7 +112,7 @@
       <nav class="sidebar sidebar-offcanvas" id="sidebar">
         <ul class="nav">
           <li class="nav-item">
-            <a class="nav-link" href="{{ route('peserta_index') }}">
+            <a class="nav-link" href="{{ route('peserta-index') }}">
               <i class="mdi mdi-home menu-icon"></i>
               <span class="menu-title">Dashboard</span>
             </a>
