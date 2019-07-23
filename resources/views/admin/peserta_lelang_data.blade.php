a3cc8f422720a94af04355e69715135969ea2805
Toms <toms@Tomss-MacBook-Pro.local>
2019-07-24 06:37:52 +0800

    add peserta lelang data view

--- resources/views/admin/peserta_lelang_data.blade.php
+++ resources/views/admin/peserta_lelang_data.blade.php
@@ -29,6 +29,7 @@
                                         <th>Nama Peserta</th>
                                         <th>No Telepon</th>
                                         <th>Pekerjaan</th>
+                                        <th>Status</th>
                                         <th>Action</th>
                                     </tr>
                                 </thead>
@@ -40,7 +41,28 @@
                                     <td>{{$datas->user->name}}</td>
                                     <td>{{$datas->telepon}}</td>
                                     <td>{{$datas->pekerjaan}}</td>
+                                    @if($datas->user->status==1)
+                                    <td  class="text-center"><label class="badge badge-info">Aktif</label></td>
+                                    @else
+                                    <td  class="text-center"><label class="badge badge-danger">Tidak Aktif</label></td>
+                                    @endif
                                     <td class="text-center">
+                                            @if($datas->user->status == 0)
+                                            <form action="{{ route('status_update', ['id' => IDCrypt::Encrypt( $datas->user->id)]) }}" method="POST">
+                                                    {{method_field('PUT') }}
+                                                    {{ csrf_field() }}
+                                                    {{-- <a href="" class="btn btn-inverse-warning" data-toggle="tooltip" data-placement="top" title="Banned" ><i class="icon-close"></i></i></a> --}}
+                                                    <button type="submit" class="btn btn-inverse-info" data-toggle="tooltip" data-placement="top" title="Aktif" name="status" value="1" ><i class=" mdi mdi-check "></i></button>
+                                                </form>
+                                            @else
+                                            <form action="{{ route('status_update', ['id' => IDCrypt::Encrypt( $datas->user->id)]) }}" method="POST">
+                                                    {{method_field('PUT') }}
+                                                    {{ csrf_field() }}
+                                                    {{-- <a href="" class="btn btn-inverse-warning" data-toggle="tooltip" data-placement="top" title="Banned" ><i class="icon-close"></i></i></a> --}}
+                                                    <button type="submit" class="btn btn-inverse-danger" data-toggle="tooltip" data-placement="top" title="Banned" name="status" value="0" ><i class="mdi mdi-close"></i></i></button>
+                                                </form>
+                                            @endif
+
                                         <a href="{{ route('peserta-lelang-detail', ['id' => IDCrypt::Encrypt( $datas->id)])}}" class="btn btn-inverse-success" style="padding:6px !important;"> <i class=" mdi mdi-eye "></i></a>
                                         <a href="{{ route('peserta-lelang-hapus', ['id' => IDCrypt::Encrypt( $datas->id)])}}" class="btn btn-inverse-danger" style="padding:6px !important;"> <i class="mdi mdi-delete"></i></a>
                                     </td>
