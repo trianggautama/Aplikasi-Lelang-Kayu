00daf085b63d187703e242243e9bd8874178954a
unknown <Windows 10 Pro@PC-.09.24.20.00>
2019-07-29 12:05:52 +0800

    fix 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -330,39 +330,19 @@
 									<div class="packages-para special-offer-para">
 										<p>
 											<span>
-											 diisi alamat
+											 Alamat <br>
+											 Jl. A.Yani Loktabat Selatan, Kecamatan Banjarbaru Selatan, Kota Banjarbaru, Provinsi Kalimantan Selatan 70714
 										</p>
 
 										<p class="offer-para">
-											Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tem ut labore et dolore magna  aliqua. Ut enim ad minim veniam, quis nostrud exercitation una <br> ullamco laboris nisi ut aliquip ex ea commodo consequat.
+											
 										</p>
 									</div><!--/.packages-para-->
-									<div class="offer-btn-group">
-										<div class="about-btn">
-											<button  class="about-view packages-btn offfer-btn">
-												make tour
-											</button>
-										</div><!--/.about-btn-->
-										<div class="about-btn">
-											<button  class="about-view packages-btn">
-												book now
-											</button>
-										</div><!--/.about-btn-->
-									</div><!--/.offer-btn-group-->
+									
 								</div><!--/.single-special-offer-txt-->
 							</div><!--/.single-special-offer-->
 						</div><!--/.col-->
-						<div class="col-sm-4">
-							<div class="single-special-offer">
-								<div class="single-special-offer-bg">
-									<img src="vendor/images/offer/offer-shape.png" alt="offer-shape">
-								</div><!--/.single-special-offer-bg-->
-								<div class="single-special-shape-txt">
-									<h3>special offer</h3>
-									<h4><span>40%</span><br>off</h4>
-									<p><span>$999</span><br>reguler $ 1450</p>
-								</div><!--/.single-special-shape-txt-->
-							</div><!--/.single-special-offer-->
+						
 						</div><!--/.col-->
 					</div><!--/.row-->
 				</div><!--/.special-offer-content-->
